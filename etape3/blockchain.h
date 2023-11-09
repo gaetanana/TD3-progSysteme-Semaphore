375676698ac91579e8dafb17a5c1c86eca1cd905
gaetanana <gaetan.gonfiantini@gmail.com>
2023-11-09 09:54:36 +0100

    Modification du blockchain.h

--- etape3/blockchain.h
+++ etape3/blockchain.h
@@ -1,28 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Structure pour une transaction
 typedef struct
 {
     long id;
     unsigned char data[256];
 } Transaction;
-
+// Structure pour un block
 typedef struct
 {
     Transaction transactions[256];
     unsigned char previous_hashcode[32];
     unsigned char hashcode[32];
 } Block;
-
 // Node for the blockchain list
 typedef struct Node
 {
     Block block;
     struct Node *next;
 } Node;
-
 // Structure for a Blockchain
 typedef struct
 {
     Node *head;
 } Blockchain;
-
-typedef unsigned char byte;
